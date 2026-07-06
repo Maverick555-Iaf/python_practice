eab766eb6f5d55c12a95053ef8d47f56300689cb
Subhodip Ghosh <subhodipghosh732@gmail.com>
2026-07-06 21:06:36 +0530

    Add files via upload

---
+++ indoor.py
@@ -0,0 +1,8 @@
+# Taking input from the user
+user_input = input("Enter something in CAPITAL: ")
+
+# Converting to small letters
+output = user_input.lower()
+
+# Printing the result
+print(output)
