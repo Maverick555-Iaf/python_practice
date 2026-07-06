a82f0f0d39cb637eefa1644d549bf305193c67a8
Subhodip Ghosh <subhodipghosh732@gmail.com>
2026-07-06 21:21:42 +0530

    Delete indoor.py

--- indoor.py
+++
@@ -1,8 +0,0 @@
-# Taking input from the user
-user_input = input("Enter something in CAPITAL: ")
-
-# Converting to small letters
-output = user_input.lower()
-
-# Printing the result
-print(output)
