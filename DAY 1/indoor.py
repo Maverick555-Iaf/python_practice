9d076b1c390e5d387aab2b025b71e7d7d79c7aba
Subhodip Ghosh <subhodipghosh732@gmail.com>
2026-07-07 21:07:55 +0530

    Update indoor.py

--- DAY 1/indoor.py
+++ DAY 1/indoor.py
@@ -2,7 +2,16 @@
 user_input = input("Enter something in CAPITAL: ")
 
 # Converting to small letters
-output = user_input.lower()
+output = user_input.lower() 
 
 # Printing the result
 print(output)
+
+
+"""
+to convert all alphabetic characters in a string to uppercase:.upper()
+Capitalizes only the very first letter of the string and makes everything else lowercase :.capitalize()
+nverts the casing, turning lowercase to uppercase and vice versa:.swapcase()
+Capitalizes the first letter of every word:.title()
+
+"""
